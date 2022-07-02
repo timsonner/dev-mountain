33744111f6f5bb14ab54f67f3533ff97fa92675b
timsonner <66705347+timsonner@users.noreply.github.com>
2022-07-02 13:13:46 -0600

    update: add completed sdlc.js
    This commit adds the completed software development lifecycle assesment.

--- week-2/assessment-js-basics-1/sdlc.js
+++ week-2/assessment-js-basics-1/sdlc.js
@@ -28,8 +28,13 @@
 
 //CODE HERE
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
-const analysisOfRequirements = `Breaks down the project into micro systems, for the purpose of analyzing project goals, focusing on what needs to be created, and allows engagement of users to understand project requirements `
-const design = ``
-const implementation = ``
-const testingAndIntegration = ``
-const maintain = ``
+
+const analysisOfRequirements = `This step breaks down the project into micro systems, for the purpose of analyzing project goals, focusing on what needs to be created, and allows engagement of users to understand project requirements.`
+
+const design = `The step where software developers define the technical details of the product. These details can include screen layouts, databases, sketches, system interfaces, prototypes, functions, operations, business rules, process diagrams, and documentation.`
+
+const implementation = `The step where the software is installed in the production environment and brought into operational status. Implementation should satisfy functional requirements, business needs, business mandates like physical constraints and service level agreements, and the project should operate as described in the user and operator manuals.`
+
+const testingAndIntegration = `The step where system integration testing is performed. This to ensures developed systems meet all technical requirements with the components and subsystems integrated. Additional tests may test security, conformance, accessibility, performance, stress, compatibility, and regression tests.`
+
+const maintain = `Maintenance of software can include upgrades, repairs, and bug fixes. Software applications typically need upgrades or integrations with new systems the customer deploys.`
