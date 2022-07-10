4f5cf23b6309b76cb0c2ed0016d6029c2608d1d0
timsonner <66705347+timsonner@users.noreply.github.com>
2022-07-10 03:42:00 -0600

    feat(week-4): add assessment cart.js
    Add function that calculates total cart price based on item totals, coupon, and sales tax. Add classes for customer, phone number, and credit card.

--- week-4/assessment-js-basics-2/cart.js
+++ week-4/assessment-js-basics-2/cart.js
@@ -39,7 +39,7 @@ const summedPrice = cart.reduce(function (previousValue, currentValue) {
     return previousValue + currentValue.price
 }, 0) // must provide initial 0 or this will NOT work (0 + 9.99, 9.99 + 8.99, etc...)
 
-console.log(summedPrice)
+console.log(`summedPrice: ${summedPrice}`)
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -57,8 +57,11 @@ console.log(summedPrice)
 */
 
 //CODE HERE
+function calcFinalPrice(cartTotal, couponValue, tax) {
+    return cartTotal * (1 + tax) - couponValue
+}
 
-
+console.log(`calcFinalPrice: ${calcFinalPrice(10, 2, .05)}`) // should be 8.5
 
 //////////////////PROBLEM 3////////////////////
 /*  
@@ -82,7 +85,22 @@ console.log(summedPrice)
 
 /*
     TEXT ANSWER HERE
+The customer object should have:
+    -name: for identification purposes
+    -phone: for delivery driver, or to let customer know takeout is ready, or to confirm reservations
+    -deliveryAddress: for delivery driver
+    -mailingAddress: for credit card validation and mailing list
+    -emailAddress: for mailing list, promotions, newsletters, and login 
+    -foodAllergies: for ensuring customer satisfaction and safety
+    -creditCard: for billing purposes
 
+    name: string, because names are characters
+    phone: phone number type, because its not exactly a number, or a string
+    deliveryAddress: string, because addresses are words and numbers
+    mailingAddress: string, because addresses are words and numbers
+    emailAddress: string, because email addresses are words and numbers
+    foodAllergies: string, because allergies are words
+    creditCard: credit card class, because its not an int, maybe a string, but can definately be broken down as its own data type a collection of ints. 
 */
 
 /*
@@ -91,3 +109,37 @@ console.log(summedPrice)
 */
 
 //CODE HERE
+class PhoneNumber {
+    constructor(country, area, prefix, line) {
+        this.country = country
+        this.area = area
+        this.prefix = prefix
+        this.line = line
+    }
+}
+
+class CreditCard {
+    constructor(first, second, third, fourth, verificationNumber) {
+        this.first = first
+        this.second = second
+        this.third = third
+        this.fourth = fourth
+        this.verificationNumber = verificationNumber
+    }
+}
+
+class Customer {
+    constructor(name, phone, deliveryAddress, mailingAddress, emailAddress, foodAllergies, creditCard) {
+this.name = name
+this.phone = phone
+this.deliveryAddress = deliveryAddress
+this.mailingAddress = mailingAddress
+this.emailAddress = emailAddress
+this.foodAllergies = foodAllergies
+this.creditCard = creditCard
+    }
+}
+
+const customer = new Customer(`Gordon Freeman`, new PhoneNumber(1,425,889,9642), `Black Mesa Research Facility, Undisclosed Location, NM 55555`, `PO BOX 1688 Bellevue, WA 98009`, `gordonf@valvesoftware.com`, `head crabs`, new CreditCard(1111, 2222, 3333, 4444, 555))
+
+console.log(customer.creditCard.typeOfCard())
