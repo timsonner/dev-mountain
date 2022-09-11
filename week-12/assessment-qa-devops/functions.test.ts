9263a4039da09c1ecdf1ed6a3faae22a494132fe
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-10 18:36:14 -0600

    refactor(week-12): format documents

--- week-12/assessment-qa-devops/functions.test.ts
+++ week-12/assessment-qa-devops/functions.test.ts
@@ -8,22 +8,19 @@ const { bots } = require('./data.js')
 // check that all the same items are in the array
 // check that the items have been shuffled around
 
-  describe('shuffleArray should', () => {
-      const foo = shuffleArray(bots)
+describe('shuffleArray should', () => {
+  const foo = shuffleArray(bots)
 
-      it('return an array', () => {
-          expect(foo).toEqual(
-            expect.arrayContaining([expect.any(Object)])
-          )    
-      }),
-          
-      it('return an array of the same length as the argument array', () => {
-        expect(foo.length).toEqual(bots.length)             
-      }),
-          it('have the same elemets in the return array as the argument array', () => {
-              expect(new Set(foo)).toEqual(new Set(bots))
-          }),
-          it('not return an array that is the same as the argument array', () => {
-              expect(foo).not.toEqual(bots)
-          })
+  it('return an array', () => {
+    expect(foo).toEqual(expect.arrayContaining([expect.any(Object)]))
+  }),
+    it('return an array of the same length as the argument array', () => {
+      expect(foo.length).toEqual(bots.length)
+    }),
+    it('have the same elemets in the return array as the argument array', () => {
+      expect(new Set(foo)).toEqual(new Set(bots))
+    }),
+    it('not return an array that is the same as the argument array', () => {
+      expect(foo).not.toEqual(bots)
+    })
 })
