70933e72275268f3e938fb77f04fa3b9e01e8675
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-19 17:46:52 -0600

    feat(week-14): add runtime analizer

--- week-14/assessment-cs/runtime.js
+++ week-14/assessment-cs/runtime.js
@@ -60,3 +60,104 @@ let resultsInsert = perf.stop();
 console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
+
+console.log(resultsInsert)
+
+
+// const util = require('util')
+// console.log(util.inspect(foo, {showHidden: false, depth: null, colors: true}))
+
+// // alternative shortcut
+// console.log(util.inspect(output, false, null, true /* enable colors */))
+
+let arrayOfResults = []
+
+const performAnalysis = (functionToTest, parameters, parameterName) => {
+    console.log()
+    perf.start()
+    functionToTest(parameters)
+    let output = perf.stop() // output becomes an object
+    output.name = `${functionToTest.name}(${parameterName})`
+    arrayOfResults.push(output) // push object to array
+}
+
+performAnalysis(doublerAppend, tinyArray, "tinyArray")
+performAnalysis(doublerInsert, tinyArray, "tinyArray")
+performAnalysis(doublerAppend, smallArray, "smallArray")
+performAnalysis(doublerInsert, smallArray, "smallArray")
+performAnalysis(doublerAppend, largeArray, "largeArray")
+performAnalysis(doublerInsert, largeArray, "largeArray")
+performAnalysis(doublerAppend, extraLargeArray, "extraLargeArray")
+performAnalysis(doublerInsert, extraLargeArray, "extraLargeArray")
+
+console.table(arrayOfResults)
+
+function convertToCSV(arrayOfObjects) {
+    const contentsOfCSV = [Object.keys(arrayOfObjects[0])].concat(arrayOfObjects)
+  
+    return contentsOfCSV.map(item => {
+      return Object.values(item).toString()
+    }).join('\n')
+}
+  
+// console.log(convertToCSV(arrayOfResults))
+
+const fs = require('fs/promises');
+
+async function writeFileToDisk(fileName, content) {
+        try {
+            fs.writeFile(fileName, content, { flag: 'a+' }, err => {
+                console.log(`error writing file ${err}`)
+            });
+            console.log(`Success!`)
+        } catch (err) {
+            console.log(err);
+          }
+}
+
+
+const readline = require('readline')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+  output: process.stdout
+})
+
+let exit = false 
+
+const firstQuestion = () => {
+    rl.question(`would you like to export these results as a CSV file? (y/n) `, (answer) => {
+        if (answer == 'y') {
+            secondQuestion()
+        } else if (answer == 'n'){
+            console.log('exiting...')
+            rl.close()
+        } else {
+            console.log(`${answer} is giberish, mate. exiting...`)
+            rl.close()
+        }
+    })
+}
+
+
+const secondQuestion = () => {
+    rl.question("Give a name and path for the file. example: ./foo.csv or /dev/nul/foo.csv ", (fileName) => {
+        rl.question(`You entered ${fileName} is this correct? (y/n) `, answer => {
+            if (answer == 'y') {
+                console.log(`Attempting to write ${fileName} to disk...`)
+                const fileContents = convertToCSV(arrayOfResults)
+                writeFileToDisk(fileName, fileContents)
+                rl.close()
+            } else if (answer == 'n'){
+                console.log('exiting...')
+                rl.close()
+            } else {
+                console.log(`${answer} is giberish, mate. exiting...`)
+                rl.close()
+            }
+        })
+    })
+}
+        
+firstQuestion()
+
