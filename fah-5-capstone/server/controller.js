cfbc0a67137bb7d870f347aff3258a05bf1b7874
timsonner <66705347+timsonner@users.noreply.github.com>
2022-09-22 18:42:47 -0600

    add spawn route

--- fah-5-capstone/server/controller.js
+++ fah-5-capstone/server/controller.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const { spawn } = require("child_process")
  
 const { createClient } = require('@supabase/supabase-js')
 
@@ -10,12 +11,46 @@ const supabase = createClient(
 
 module.exports = {
     get: async (req, res) => {
-try {
-    const { data: commands, error } = await supabase.from('commands').select('*')
-    console.log(commands)
-} catch (error) {
-    console.log(error)
-}
-    }
+        try {
+            const { data: commands, error } = await supabase.from('commands').select('*')
+            console.log(commands)
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    spawn: async (req, res) => {
 
+        let output = ''
+        const makeSystemCall = (command) => {
+            const systemCall = spawn(command)
+        
+            systemCall.stdout.on(`data`, (data) => {
+                output += data
+                console.log(`stdout: ${data}`)
+            })
+        
+            systemCall.stderr.on(`data`, (data) => {
+                console.error(`stderr: ${data}`)
+            })
+        
+            systemCall.on(`close`, (code) => {
+                console.log(`child process exited with code ${code}`)
+            })
+        }
+
+        try {
+            const { data: commands, error } = await supabase.from('commands').select('*')
+            console.log(commands)
+        } catch (error) {
+            console.log(error)
+        }
+    makeSystemCall('ls')
+
+    }
 }
+
+
+
+
+
+
