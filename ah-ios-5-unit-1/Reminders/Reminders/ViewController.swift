413aa751d5ed9c52770497207fb4cffa7a061232
timsonner <66705347+timsonner@users.noreply.github.com>
2022-10-19 22:49:29 -0600

    rename view controller class and change superclass

---
+++ ah-ios-5-unit-1/Reminders/Reminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Timothy Sonner on 10/19/22.
+//
+
+import Foundation
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
