9e9fcf89e25e42a64138f38de47248b9e108f256
timsonner <66705347+timsonner@users.noreply.github.com>
2022-10-27 23:39:09 -0600

    add table view

---
+++ ah-ios-5-unit-1/Simpons Book/DetailsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailsViewController.swift
+//  Simpons Book
+//
+//  Created by Timothy Sonner on 10/27/22.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var jobLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
